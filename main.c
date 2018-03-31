bb2bb2f8dcf4a2a1f0dd6d785788b21a2010b638
aslin1126 <63052533@qq.com>
2018-03-31 23:41:55 +0800

    fix some error

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <linux/types.h>
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #define MAJOR_NUMBER 61
 
 /* forward declaration */
@@ -42,8 +42,8 @@ ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 	
 	copy_to_user(buf, onebyte_data, 1);
 	
-	bytes_read ++;
-	return bytes_read;	
+	readbytes ++;
+	return readbytes;	
 }
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
