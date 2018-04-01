86aedb6d4e6bd96b424afd9fcf408f4fc5f8104a
aslin1126 <63052533@qq.com>
2018-04-01 12:38:07 +0800

    add printk messages

--- main.c
+++ main.c
@@ -7,16 +7,18 @@
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
-#define MAJOR_NUMBER 61
 
-/* forward declaration */
+
+#define MAJOR_NUMBER 61/* forward declaration */
+
+
 int onebyte_open(struct inode *inode, struct file *filep);
 int onebyte_release(struct inode *inode, struct file *filep);
-ssize_t onebyte_read(struct file *filep, char *buf, size_t
-count, loff_t *f_pos);
-ssize_t onebyte_write(struct file *filep, const char *buf,
-size_t count, loff_t *f_pos);
+ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos);
+ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos);
 static void onebyte_exit(void);
+
+
 /* definition of file_operation structure */
 struct file_operations onebyte_fops = {
 	read: onebyte_read,
@@ -24,7 +26,9 @@ struct file_operations onebyte_fops = {
 	open: onebyte_open,
 	release: onebyte_release
 };
+
 char *onebyte_data = NULL;
+
 int onebyte_open(struct inode *inode, struct file *filep)
 {
 	return 0; // always successful
@@ -38,30 +42,32 @@ ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 	if(*buf != 0){
 		return 0;	
 	}
-	int readbytes= 0;
-	
-	copy_to_user(buf, onebyte_data, 1);
+	int readbytes = 0;
+	copy_to_user(buf, onebyte_data, sizeof(char));
+
 	
 	readbytes ++;
-	return readbytes;	
+	printk(KERN_ALERT "Read %d number of bytes\n", readbytes);
+	return readbytes;
 }
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
-	if(*buf != 0){
-		return 0;	
+	int writebytes = 0;	
+	if(count > 1){
+		printk(KERN_ALERT "Write more than 1 byte \n");	
 	}
-	int writebytes= 0;
-	copy_from_user(onebyte_data, buf, 1);
 
-	writebytes++;
-  	return writebytes;
+	copy_from_user(onebyte_data, buf, sizeof(char));
+	writebytes ++;
+	printk(KERN_ALERT "Write %d number of bytes\n", writebytes);
+	return writebytes;
 }
+
 static int onebyte_init(void)
 {
 	int result;
 	// register the device
-	result = register_chrdev(MAJOR_NUMBER, "onebyte",
-	&onebyte_fops);
+	result = register_chrdev(MAJOR_NUMBER, "onebyte", &onebyte_fops);
 	if (result < 0) {
 		return result;
 	}
@@ -70,30 +76,36 @@ static int onebyte_init(void)
 	// the type of memory to be allocated.
 	// To release the memory allocated by kmalloc, use kfree.
 	onebyte_data = kmalloc(sizeof(char), GFP_KERNEL);
+
 	if (!onebyte_data) {
 		onebyte_exit();
 		// cannot allocate memory
-		// return no memory error, negative signify a
-		failure
+		// return no memory error, negative signify a failure
 		return -ENOMEM;
 	}
+
 	// initialize the value to be X
 	*onebyte_data = 'X';
+
 	printk(KERN_ALERT "This is a onebyte device module\n");
 	return 0;
 }
+
+
 static void onebyte_exit(void)
 {
 	// if the pointer is pointing to something
 	if (onebyte_data) {
-	// free the memory and assign the pointer to NULL
-	kfree(onebyte_data);
-	onebyte_data = NULL;
+		// free the memory and assign the pointer to NULL
+		kfree(onebyte_data);
+		onebyte_data = NULL;
 	}
+
 	// unregister the device
 	unregister_chrdev(MAJOR_NUMBER, "onebyte");
 	printk(KERN_ALERT "Onebyte device module is unloaded\n");
 }
+
 MODULE_LICENSE("GPL");
 module_init(onebyte_init);
 module_exit(onebyte_exit);
